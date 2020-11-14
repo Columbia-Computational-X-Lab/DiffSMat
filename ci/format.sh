1f6f629b86a13e68436d38ac818b6f7302573e9f
Changxi Zheng <cxz@cs.columbia.edu>
2020-11-13 23:52:45 -0500

    added license header

--- ci/format.sh
+++ ci/format.sh
@@ -5,3 +5,13 @@ for f in $(find src -iname "*.h" -o -iname "*.hpp" -o -iname "*.cpp" -o -iname "
     expand -t 4 $f > /tmp/e
     mv -f /tmp/e $f
 done
+
+for f in $(find src -iname "*.h" -o -iname "*.hpp" -o -iname "*.cpp" -o -iname "*.c"); do 
+    if ! grep -q License $f
+    then
+        cat lic.txt $f > /tmp/e
+        mv -f /tmp/e $f
+    else
+        echo "License exists in $f"
+    fi
+done
